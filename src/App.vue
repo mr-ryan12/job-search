a06625b9b27210f936ca730ae544cd6ed4ff0315
Ryan McBride <mr.ryan444@gmail.com>
2022-06-11 13:32:18 -0600

    Add nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <template>
-  <h1>Hello Vue</h1>
+  <h1 class="mt-8 text-6xl text-center">Hello Vue</h1>
+  <MainNav />
 </template>
 
 <script>
+import MainNav from "@/components/MainNav.vue";
+
 export default {
   name: "App",
+  components: {
+    MainNav,
+  },
 };
 </script>
---
+++ src/components/MainNav.vue
@@ -0,0 +1,35 @@
+<template>
+  <nav>
+    <h2 class="nav-logo">
+      <span class="text-blue-500">G</span>
+      <span class="text-red-500">o</span>
+      <span class="text-yellow-500">o</span>
+      <span class="text-blue-500">g</span>
+      <span class="text-green-500">l</span>
+      <span class="text-red-500">e</span>
+      Careers
+    </h2>
+    <p class="sub-nav">Teams</p>
+    <p class="sub-nav">Locations</p>
+    <p class="sub-nav">Students</p>
+    <p class="sub-nav">Jobs</p>
+  </nav>
+</template>
+
+<style scoped>
+nav {
+  display: flex;
+  align-items: center;
+  border: 2px solid black;
+  padding: 0.5rem;
+}
+
+.nav-logo {
+  font-size: 1.25rem;
+}
+
+nav > h2,
+nav > p {
+  margin: 10px;
+}
+</style>
