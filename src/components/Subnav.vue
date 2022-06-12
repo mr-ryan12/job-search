ee5efe7307345e1609a3dcebcf1b6b45876a43f0
Ryan McBride <mr.ryan444@gmail.com>
2022-06-12 17:29:26 -0600

    Style subnav

--- src/components/Subnav.vue
+++ src/components/Subnav.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <h3>this is a subnav</h3>
+  <div class="main-container">
+    <div class="sub-container">this is a subnav</div>
   </div>
 </template>
 
@@ -9,3 +9,20 @@ export default {
   name: "Subnav",
 };
 </script>
+
+<style lang="scss" scoped>
+.main-container {
+  width: 100%;
+  height: 4rem;
+  background-color: #fff;
+  border-bottom: 1px solid #dadcd0;
+}
+
+.sub-container {
+  display: flex;
+  align-items: center;
+  height: 100%;
+  padding-left: 2rem;
+  padding-right: 2rem;
+}
+</style>
