737ee0c09b7bd68a19126830f538b57bb76e6776
Ryan McBride <mr.ryan444@gmail.com>
2022-06-12 15:06:57 -0600

    Add sass

--- src/components/ActionButton.vue
+++ src/components/ActionButton.vue
@@ -1,10 +1,5 @@
 <template>
-  <button
-    data-test="sign-in-button"
-    :class="isHovering ? 'hover' : null"
-    @mouseenter="hoverTrue"
-    @mouseleave="hoverFalse"
-  >
+  <button data-test="sign-in-button" :class="buttonStyle">
     {{ text }}
   </button>
 </template>
@@ -16,24 +11,20 @@ export default {
       type: String,
       default: "",
     },
+    buttonStyle: {
+      type: String,
+      default: "",
+    },
   },
   data() {
     return {
       isHovering: false,
     };
   },
-  methods: {
-    hoverTrue() {
-      this.isHovering = true;
-    },
-    hoverFalse() {
-      this.isHovering = false;
-    },
-  },
 };
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 button {
   background-color: blue;
   color: #fff;
@@ -43,9 +34,12 @@ button {
   margin-top: 1rem;
   font-size: 0.8rem;
   border-radius: 5px;
+  &:hover {
+    box-shadow: 0 0 3px 3px rgb(137, 137, 238);
+  }
 }
 
-.hover {
-  box-shadow: 0 0 3px 3px rgb(137, 137, 238);
-}
+// .hover {
+//   box-shadow: 0 0 3px 3px rgb(137, 137, 238);
+// }
 </style>
