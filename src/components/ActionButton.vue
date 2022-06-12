148315c002a7264763fb2e95f7fb15762a040de4
Ryan McBride <mr.ryan444@gmail.com>
2022-06-12 16:18:56 -0600

    Update computed property

--- src/components/ActionButton.vue
+++ src/components/ActionButton.vue
@@ -19,8 +19,7 @@ export default {
   computed: {
     buttonClass() {
       return {
-        primary: this.type === "primary",
-        secondary: this.type === "secondary",
+        [this.type]: true,
       };
     },
   },
