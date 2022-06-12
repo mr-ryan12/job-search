56afdc65030fbafcc1d3812a755edbc321b58b02
Ryan McBride <mr.ryan444@gmail.com>
2022-06-12 16:40:36 -0600

    Add and pass ActionButton tests

---
+++ tests/unit/components/ActionButton.test.js
@@ -0,0 +1,25 @@
+import { mount } from "@vue/test-utils";
+
+import ActionButton from "@/components/ActionButton.vue";
+
+describe("Action Button", () => {
+  it("Should render text", () => {
+    const wrapper = mount(ActionButton, {
+      props: {
+        text: "Something something dark side",
+        type: "Primary",
+      },
+    });
+    expect(wrapper.text()).toMatch("Something something dark side");
+  });
+
+  it("Should have a primary class", () => {
+    const wrapper = mount(ActionButton, {
+      props: {
+        type: "primary",
+      },
+    });
+    const actionButton = wrapper.find("button");
+    expect(actionButton.classes("primary")).toBe(true);
+  });
+});
