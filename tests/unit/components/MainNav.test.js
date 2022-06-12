2098ab1560637d3a059a822931c0a6247b8890da
Ryan McBride <mr.ryan444@gmail.com>
2022-06-12 13:55:22 -0600

    Add trigger method in login test

--- tests/unit/components/MainNav.test.js
+++ tests/unit/components/MainNav.test.js
@@ -35,24 +35,23 @@ describe("MainNav", () => {
   describe("When user is logged out", () => {
     it("Prompts user to sign in", () => {
       const loginButton = wrapper.find("[data-test='sign-in-button']");
-      const profileImage = wrapper.find("[data-test='profile-image']");
-
       expect(loginButton.exists()).toBe(true);
-      expect(profileImage.exists()).toBe(false);
     });
   });
 
   describe("When user is logged in", () => {
-    it("Should display a profile image", () => {
-      const wrapper = mount(App, {
-        data() {
-          return {
-            isLoggedIn: true,
-          };
-        },
-      });
-      const loginButton = wrapper.find("[data-test='sign-in-button']");
-      const profileImage = wrapper.find("[data-test='profile-image']");
+    it("Should display a profile image", async () => {
+      const wrapper = mount(App);
+      let loginButton = wrapper.find("[data-test='sign-in-button']");
+      let profileImage = wrapper.find("[data-test='profile-image']");
+
+      expect(loginButton.exists()).toBe(true);
+      expect(profileImage.exists()).toBe(false);
+
+      await loginButton.trigger("click");
+
+      loginButton = wrapper.find("[data-test='sign-in-button']");
+      profileImage = wrapper.find("[data-test='profile-image']");
 
       expect(loginButton.exists()).toBe(false);
       expect(profileImage.exists()).toBe(true);
