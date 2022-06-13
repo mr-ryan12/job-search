a1e1376620499914490b3aba2eb67cb9ec23ac72
Ryan McBride <mr.ryan444@gmail.com>
2022-06-13 15:51:57 -0600

    Fix failing test

--- src/utils/nextElementInList.js
+++ src/utils/nextElementInList.js
@@ -1,6 +1,6 @@
 const nextElementInList = (list, value) => {
   const actionIndex = list.indexOf(value);
-  const newIndex = (actionIndex + 1) % 4;
+  const newIndex = (actionIndex + 1) % list.length;
 
   return list[newIndex];
 };
--- tests/unit/utils/nextElementInList.test.js
+++ tests/unit/utils/nextElementInList.test.js
@@ -8,4 +8,14 @@ describe("Next element in list", () => {
 
     expect(result).toBe("D");
   });
+
+  describe("When element is the last in the list", () => {
+    it("Should return the first element in the array", () => {
+      const list = ["A", "B", "C", "D", "E"];
+      const value = "E";
+      const result = nextElementInList(list, value);
+
+      expect(result).toBe("A");
+    });
+  });
 });
