a4d40b6607125c36997cbb5cfd9d726f5bd9eca0
Ryan McBride <mr.ryan444@gmail.com>
2022-06-13 15:19:57 -0600

    Change action

---
+++ src/components/Headline.vue
@@ -0,0 +1,36 @@
+<template>
+  <section>
+    <h1>{{ action }} for everyone</h1>
+    <h2>Find your next job at Trey's Corp</h2>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "HeadLine",
+  data() {
+    return {
+      sample: "Hello! Thank you for stopping by!",
+      action: "Build",
+      interval: null,
+    };
+  },
+  created() {
+    this.changeAction();
+  },
+  beforeUnmount() {
+    clearInterval(this.interval);
+  },
+  methods: {
+    changeAction() {
+      this.interval = setInterval(() => {
+        const actions = ["Build", "Create", "Design", "Code"];
+        const actionIndex = actions.indexOf(this.action);
+        const newIndex = (actionIndex + 1) % 4;
+
+        this.action = actions[newIndex];
+      }, 3000);
+    },
+  },
+};
+</script>
---
+++ src/playground.js
@@ -0,0 +1,11 @@
+// setTimeout(() => {
+//   console.log("I will print two seconds after the program starts");
+// }, 2000);
+
+// const interval = setInterval(() => {
+//   console.log("I will print every two seconds");
+// }, 2000);
+
+// setTimeout(() => {
+//   clearInterval(interval);
+// }, 10000);
