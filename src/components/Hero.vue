407a1d06710f7343e0ce8859b52510decaf7a711
Ryan McBride <mr.ryan444@gmail.com>
2022-06-12 19:50:18 -0600

    Style main section

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,9 +1,20 @@
 <template>
   <main>
-    <section>I can be your hero baby</section>
+    <section class="hero">I can be your hero baby</section>
   </main>
 </template>
 
 <script>
 export default {};
 </script>
+
+<style lang="scss" scoped>
+.hero {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  padding-top: 10px;
+  padding-bottom: 80px;
+  background-color: yellow;
+}
+</style>
