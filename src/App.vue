08c87f97d260411862e17fa33f242efbb45d8a12
Ryan McBride <mr.ryan444@gmail.com>
2022-06-14 12:52:56 -0600

    Install router

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,30 @@
 <template>
-  <div>
-    <MainNav />
-    <Hero />
-  </div>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view />
 </template>
 
-<script>
-import MainNav from "@/components/navigation/MainNav.vue";
-import Hero from "@/components/JobSearch/Hero.vue";
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 
-export default {
-  name: "App",
-  components: {
-    MainNav,
-    Hero,
-  },
-};
-</script>
+nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
