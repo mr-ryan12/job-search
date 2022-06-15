fcc024b19eebee2b9133184625b3ba8bb2a7efda
Ryan McBride <mr.ryan444@gmail.com>
2022-06-14 22:45:54 -0600

    Add and pass tests

--- tests/unit/components/JobResults/JobListing.test.js
+++ tests/unit/components/JobResults/JobListing.test.js
@@ -3,7 +3,14 @@ import { mount, RouterLinkStub } from "@vue/test-utils";
 import JobResults from "@/components/JobResults/JobListing.vue";
 
 describe("Job Results", () => {
-  const createConfig = (title) => ({
+  const createJobProps = (jobProps = {}) => ({
+    title: "Vue Developer",
+    organization: "Air BnB",
+    locations: ["Minneapolis"],
+    minimumQualifications: ["Succeed"]
+    ...jobProps,
+  });
+  const createConfig = (jobProps) => ({
     global: {
       stubs: {
         RouterLink: RouterLinkStub,
@@ -11,17 +18,40 @@ describe("Job Results", () => {
     },
     props: {
       job: {
-        title: title,
+        ...jobProps,
       },
     },
   });
   it("Should display a title", () => {
-    const wrapper = mount(JobResults, createConfig("Vue Developer"));
-    expect(wrapper.text()).toMatch("Vue Developer");
+    const jobProps = createJobProps({ title: "Vue Programmer" });
+    const wrapper = mount(JobResults, createConfig(jobProps));
+    expect(wrapper.text()).toMatch("Vue Programmer");
   });
 
   it("Should display a organization", () => {
-    const wrapper = mount(JobResults, createConfig("Air BnB"));
-    expect(wrapper.text()).toMatch("Air BnB");
+    const jobProps = createJobProps({ organization: "Vue need some food" });
+    const wrapper = mount(JobResults, createConfig(jobProps));
+    expect(wrapper.text()).toMatch("Vue need some food");
+  });
+
+  it("Should have locations", () => {
+    const jobProps = createJobProps({ locations: ["Dallas", "New York"] });
+    const wrapper = mount(JobResults, createConfig(jobProps));
+    expect(wrapper.text()).toMatch("Dallas");
+    expect(wrapper.text()).toMatch("New York");
+  });
+
+  it("Should have qualifications", () => {
+    const jobProps = createJobProps({
+      minimumQualifications: [
+        "Excellent attention to detail",
+        "Willing to learn new tech",
+        "Team player",
+      ],
+    });
+    const wrapper = mount(JobResults, createConfig(jobProps));
+    expect(wrapper.text()).toMatch("Excellent attention to detail");
+    expect(wrapper.text()).toMatch("Willing to learn new tech");
+    expect(wrapper.text()).toMatch("Team player");
   });
 });
