1d51fbe2e26ea6038e8fd8ef3b5672347d1146c1
Ryan McBride <mr.ryan444@gmail.com>
2022-06-15 17:45:13 -0600

    Refactor test suite

--- tests/unit/components/JobResults/JobListings.test.js
+++ tests/unit/components/JobResults/JobListings.test.js
@@ -5,6 +5,13 @@ jest.mock("axios");
 import JobListings from "@/components/JobResults/JobListings.vue";
 
 describe("JobListings", () => {
+  beforeEach(() => {
+    axios.get.mockResolvedValue({ data: Array(15).fill({}) });
+  });
+
+  afterEach(() => {
+    axios.get.mockReset();
+  });
   const createRoute = (queryParams = {}) => ({
     query: {
       page: "5",
@@ -29,12 +36,29 @@ describe("JobListings", () => {
   });
 
   it("Should create job listings for a maximum of 10 jobs", async () => {
-    axios.get.mockResolvedValue({ data: Array(15).fill({}) });
     const queryParams = { page: "1" };
     const $route = createRoute(queryParams);
     const wrapper = shallowMount(JobListings, createConfig($route));
     await flushPromises();
     const jobListings = wrapper.findAll("[data-test='job-listing']");
     expect(jobListings).toHaveLength(10);
+  });
+
+  describe("When query params exclude page number", () => {
+    it("Should display page #1", () => {
+      const queryParams = { page: undefined };
+      const $route = createRoute(queryParams);
+      const wrapper = shallowMount(JobListings, createConfig($route));
+      expect(wrapper.text()).toMatch("Page 1");
+    });
+  });
+
+  describe("When query params include page number", () => {
+    it("Should display page number", () => {
+      const queryParams = { page: 3 };
+      const $route = createRoute(queryParams);
+      const wrapper = shallowMount(JobListings, createConfig($route));
+      expect(wrapper.text()).toMatch("Page 3");
+    });
   });
 });
