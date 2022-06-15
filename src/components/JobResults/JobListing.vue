11b18dfc298438035e23dd9b425e03a4d8a00265
Ryan McBride <mr.ryan444@gmail.com>
2022-06-14 20:05:29 -0600

    Create JobListing component

---
+++ src/components/JobResults/JobListing.vue
@@ -0,0 +1,112 @@
+<template>
+  <div>
+    <li>
+      <RouterLink to="/jobs/results/1" class="link">
+        <div class="job-title-container">
+          <h2 class="job-title">
+            Technical Program Manager, Perception, Augmented Reality
+          </h2>
+          <div class="company-info">
+            <div class="company-title">
+              <span>Trey Trey</span>
+            </div>
+            <div>
+              <span>Denver, CO, USA</span>
+            </div>
+          </div>
+        </div>
+        <div class="change-this">
+          <div class="qualifications-container">
+            <h3 class="qualifications-title">Qualifications:</h3>
+            <div>
+              <ul class="qualifications-list">
+                <li>Bachelor's degree or equivalent practical experience.</li>
+                <li>5 years of experience in program management.</li>
+                <li>
+                  Experience analyzing data through querying database (e.g SQL),
+                  using spreadsheet software, and creating statistical models.
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="expand-container">
+            <RouterLink to="/jobs/results/1" class="expand">Expand</RouterLink>
+          </div>
+        </div>
+      </RouterLink>
+    </li>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "JobListing",
+};
+</script>
+
+<style lang="scss" scoped>
+li {
+  margin-bottom: 0.25rem;
+}
+
+.link {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  background-color: #fff;
+  border: 1px solid #f8f9fa;
+  border-radius: 0.25rem;
+  &:hover {
+    box-shadow: 0 1px 3px 0 rgba(60, 64, 67, 0.3);
+  }
+}
+
+.job-title-container {
+  padding-top: 1.25rem;
+  padding-bottom: 0.5rem;
+  margin-left: 2rem;
+  margin-right: 2rem;
+  border-bottom: 1px solid #f8f9fa;
+}
+
+.job-title {
+  margin-bottom: 0.5rem;
+  font-size: 1.5rem;
+  line-height: 2rem;
+}
+
+.company-title {
+  margin-right: 1.25rem;
+}
+
+.company-info {
+  display: flex;
+  vertical-align: middle;
+}
+
+.change-this {
+  padding-left: 2rem;
+  padding-right: 2rem;
+  padding-top: 1rem;
+  padding-bottom: 1rem;
+}
+
+.qualifications-title {
+  margin-top: 0.25rem;
+  margin-bottom: 0.5rem;
+}
+
+.qualifications-list {
+  padding-left: 2rem;
+  list-style-type: disc;
+}
+
+.expand-container {
+  margin-top: 0.5rem;
+  text-align: center;
+}
+
+.expand {
+  color: #1967d2;
+}
+</style>
