782680f035d980749d62f8d1ffdb9a607341d387
Ryan McBride <mr.ryan444@gmail.com>
2022-06-15 17:23:00 -0600

    Add previous and next links

--- src/components/JobResults/JobListings.vue
+++ src/components/JobResults/JobListings.vue
@@ -8,6 +8,25 @@
         data-test="job-listing"
       />
     </ol>
+    <div class="page-link-container">
+      <div class="page-link-sub-container">
+        <p class="page-link">Page {{ currentPage }}</p>
+        <div class="router-link-container">
+          <RouterLink
+            v-if="previousPage"
+            :to="{ name: 'JobResults', query: { page: previousPage } }"
+            class="previous"
+            >Previous</RouterLink
+          >
+          <RouterLink
+            v-if="nextPage"
+            :to="{ name: 'JobResults', query: { page: nextPage } }"
+            class="next"
+            >Next</RouterLink
+          >
+        </div>
+      </div>
+    </div>
   </main>
 </template>
 
@@ -27,9 +46,23 @@ export default {
     };
   },
   computed: {
+    currentPage() {
+      const pageString = this.$route.query.page || 1;
+      return Number.parseInt(pageString);
+    },
+    previousPage() {
+      const previousPage = this.currentPage - 1;
+      const firstPage = 1;
+      return previousPage >= firstPage ? previousPage : undefined;
+    },
+    nextPage() {
+      const nextPage = this.currentPage + 1;
+      const maxPage = this.jobs.length / 10;
+
+      return nextPage <= maxPage ? nextPage : undefined;
+    },
     displayedJobs() {
-      const pageNumberString = this.$route.query.page || 1;
-      const pageNumber = Number.parseInt(pageNumberString);
+      const pageNumber = this.currentPage;
       const firstIndex = (pageNumber - 1) * 10;
       const lastIndex = pageNumber * 10;
       return this.jobs.slice(firstIndex, lastIndex);
@@ -47,5 +80,38 @@ export default {
   flex: 1 1 auto;
   padding: 2rem;
   background-color: #f8f9fa;
+}
+
+.page-link-container {
+  margin-top: 1.75rem;
+  margin-right: auto;
+  margin-left: auto;
+}
+
+.page-link-sub-container {
+  display: flex;
+  flex-wrap: nowrap;
+}
+
+.page-link {
+  font-size: 0.875rem;
+  line-height: 1.25rem;
+  flex-grow: 1;
+}
+
+.router-link-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.previous,
+.next {
+  margin-left: 0.75rem;
+  margin-right: 0.75rem;
+  font-size: 0.875rem;
+  line-height: 1.25rem;
+  font-weight: 600;
+  color: #1967d2;
 }
 </style>
