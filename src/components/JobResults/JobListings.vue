88aed9f4699c26706df9e3fa2e505d863103579c
Ryan McBride <mr.ryan444@gmail.com>
2022-06-14 19:01:38 -0600

    Add JobListings component to screen

---
+++ src/components/JobResults/JobListings.vue
@@ -0,0 +1,9 @@
+<template>
+  <main>Doin stuff with the things</main>
+</template>
+
+<script>
+export default {
+  name: "JobListings",
+};
+</script>
--- src/views/JobResultsView.vue
+++ src/views/JobResultsView.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
     <JobFiltersSidebar />
+    <JobListings />
   </div>
 </template>
 
 <script>
 import JobFiltersSidebar from "@/components/JobResults/JobFiltersSidebar/JobFiltersSidebar.vue";
+import JobListings from "@/components/JobResults/JobListings.vue";
 export default {
   name: "JobResultsView",
   components: {
     JobFiltersSidebar,
+    JobListings,
   },
 };
 </script>
