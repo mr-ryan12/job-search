fc5601a1f904a2a435a259543633868817f02153
Ryan McBride <mr.ryan444@gmail.com>
2022-06-14 22:25:25 -0600

    Add and pass JobListing tests

--- tests/unit/components/JobResults/JobListing.test.js
+++ tests/unit/components/JobResults/JobListing.test.js
@@ -3,20 +3,25 @@ import { mount, RouterLinkStub } from "@vue/test-utils";
 import JobResults from "@/components/JobResults/JobListing.vue";
 
 describe("Job Results", () => {
+  const createConfig = (title) => ({
+    global: {
+      stubs: {
+        RouterLink: RouterLinkStub,
+      },
+    },
+    props: {
+      job: {
+        title: title,
+      },
+    },
+  });
   it("Should display a title", () => {
-
-    const wrapper = mount(JobResults, {
-      global: {
-        stubs: {
-          RouterLink: RouterLinkStub
-        }
-      },
-      props: {
-        job: {
-          title: "Vue Developer",
-        },
-      },
-    });
+    const wrapper = mount(JobResults, createConfig("Vue Developer"));
     expect(wrapper.text()).toMatch("Vue Developer");
+  });
+
+  it("Should display a organization", () => {
+    const wrapper = mount(JobResults, createConfig("Air BnB"));
+    expect(wrapper.text()).toMatch("Air BnB");
   });
 });
