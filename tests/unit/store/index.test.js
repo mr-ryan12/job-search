4d719b4d0be0d927f0380616ecec663975b1ab5c
Ryan McBride <mr.ryan444@gmail.com>
2022-06-16 12:44:12 -0600

    Add and pass commit test

--- tests/unit/store/index.test.js
+++ tests/unit/store/index.test.js
@@ -1,4 +1,6 @@
-import { state, mutations } from "@/store";
+import { state, mutations, actions } from "@/store";
+import getJobs from "@/api/getJobs";
+jest.mock("@/api/getJobs");
 
 describe("State", () => {
   it("Should have a login property", () => {
@@ -27,5 +29,31 @@ describe("mutations", () => {
       mutations.RECEIVE_JOBS(state, ["Job 1", "Job 2"]);
       expect(state.jobs).toEqual(["Job 1", "Job 2"]);
     });
+  });
+});
+
+describe("Actions", () => {
+  beforeEach(() => {
+    getJobs.mockResolvedValue([
+      {
+        id: 1,
+        title: "Vue Developer",
+      },
+    ]);
+  });
+
+  it("Should make a request to fetch jobs", async () => {
+    const context = { commit: jest.fn() };
+    await actions.FETCH_JOBS(context);
+    expect(getJobs).toHaveBeenCalled();
+  });
+
+  it("Should send message to save received jobs in store", async () => {
+    const commit = jest.fn();
+    const context = { commit };
+    await actions.FETCH_JOBS(context);
+    expect(commit).toHaveBeenCalledWith("RECEIVE_JOBS", [
+      { id: 1, title: "Vue Developer" },
+    ]);
   });
 });
