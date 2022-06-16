1e2948f3265cba1842d7967f3493638a28888b98
Ryan McBride <mr.ryan444@gmail.com>
2022-06-16 17:37:29 -0600

    Pass failing tests

--- tests/unit/components/JobResults/JobListings.test.js
+++ tests/unit/components/JobResults/JobListings.test.js
@@ -36,47 +36,77 @@ describe("JobListings", () => {
     expect(jobListings).toHaveLength(10);
   });
 
-  describe.skip("When query params exclude page number", () => {
+  describe("When query params exclude page number", () => {
     it("Should display page #1", () => {
       const queryParams = { page: undefined };
       const $route = createRoute(queryParams);
-      const wrapper = shallowMount(JobListings, createConfig($route));
+      const $store = {
+        state: {
+          jobs: Array(15).fill({}),
+        },
+        dispatch: jest.fn(),
+      };
+      const wrapper = shallowMount(JobListings, createConfig($route, $store));
       expect(wrapper.text()).toMatch("Page 1");
     });
   });
 
-  describe.skip("When query params include page number", () => {
+  describe("When query params include page number", () => {
     it("Should display page number", () => {
       const queryParams = { page: 3 };
       const $route = createRoute(queryParams);
-      const wrapper = shallowMount(JobListings, createConfig($route));
+      const $store = {
+        state: {
+          jobs: Array(15).fill({}),
+        },
+        dispatch: jest.fn(),
+      };
+      const wrapper = shallowMount(JobListings, createConfig($route, $store));
       expect(wrapper.text()).toMatch("Page 3");
     });
   });
 
-  describe.skip("When user is on first page of job results", () => {
+  describe("When user is on first page of job results", () => {
     it("Does not show link to previous page", () => {
       const queryParams = { page: 1 };
       const $route = createRoute(queryParams);
-      const wrapper = shallowMount(JobListings, createConfig($route));
+      const $store = {
+        state: {
+          jobs: Array(15).fill({}),
+        },
+        dispatch: jest.fn(),
+      };
+      const wrapper = shallowMount(JobListings, createConfig($route, $store));
       const previousPage = wrapper.find("[data-test='previous-page-link']");
       expect(previousPage.exists()).toBe(false);
     });
     it("Shows link to next page", async () => {
       const queryParams = { page: 1 };
       const $route = createRoute(queryParams);
-      const wrapper = shallowMount(JobListings, createConfig($route));
+      const $store = {
+        state: {
+          jobs: Array(15).fill({}),
+        },
+        dispatch: jest.fn(),
+      };
+      const wrapper = shallowMount(JobListings, createConfig($route, $store));
       await flushPromises();
       const nextPage = wrapper.find("[data-test='next-page-link']");
       expect(nextPage.exists()).toBe(true);
     });
   });
 
-  describe.skip("When user is on last page of job results", () => {
+  describe("When user is on last page of job results", () => {
     it("Does not show link to next page", async () => {
       const queryParams = { page: "2" };
       const $route = createRoute(queryParams);
-      const wrapper = shallowMount(JobListings, createConfig($route));
+      const $store = {
+        state: {
+          jobs: Array(15).fill({}),
+        },
+        dispatch: jest.fn(),
+      };
+      const wrapper = shallowMount(JobListings, createConfig($route, $store));
       await flushPromises();
       const nextPage = wrapper.find("[data-test='next-page-link']");
       expect(nextPage.exists()).toBe(false);
@@ -84,7 +114,13 @@ describe("JobListings", () => {
     it("Shows link to previous page", async () => {
       const queryParams = { page: "2" };
       const $route = createRoute(queryParams);
-      const wrapper = shallowMount(JobListings, createConfig($route));
+      const $store = {
+        state: {
+          jobs: Array(15).fill({}),
+        },
+        dispatch: jest.fn(),
+      };
+      const wrapper = shallowMount(JobListings, createConfig($route, $store));
       await flushPromises();
       const previousPage = wrapper.find("[data-test='previous-page-link']");
       expect(previousPage.exists()).toBe(true);
