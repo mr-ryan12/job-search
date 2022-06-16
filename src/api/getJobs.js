782e550fbbf8c55ded90f5a32d7e3f900539d6c6
Ryan McBride <mr.ryan444@gmail.com>
2022-06-16 11:42:29 -0600

    Add and pass fetch request test

--- src/api/getJobs.js
+++ src/api/getJobs.js
@@ -2,7 +2,9 @@ import axios from "axios";
 
 const getJobs = async () => {
   const baseUrl = process.env.VUE_APP_API_URL;
-  await axios.get(`${baseUrl}/jobs`);
+  const response = await axios.get(`${baseUrl}/jobs`);
+
+  return response.data;
 };
 
 export default getJobs;
--- tests/unit/api/getJobs.test.js
+++ tests/unit/api/getJobs.test.js
@@ -4,8 +4,30 @@ jest.mock("axios");
 import getJobs from "@/api/getJobs";
 
 describe("getJobs", () => {
+  beforeEach(() => {
+    axios.get.mockResolvedValue({
+      data: [
+        {
+          id: 1,
+          title: "Vue Developer",
+        },
+      ],
+    });
+  });
+
   it("Should fetch jobs", async () => {
     await getJobs();
     expect(axios.get).toHaveBeenCalledWith("http://mytotallyfakeapi.com/jobs");
+  });
+
+  it("Should extract jobs from response", async () => {
+    const response = await getJobs();
+
+    expect(response).toEqual([
+      {
+        id: 1,
+        title: "Vue Developer",
+      },
+    ]);
   });
 });
