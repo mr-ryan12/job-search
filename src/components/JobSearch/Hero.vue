cdd0b37287ef2d239cf419a24f1e27c5a56d3a67
Ryan McBride <mr.ryan444@gmail.com>
2022-06-17 15:43:45 -0600

    Render spotlight components

--- src/components/JobSearch/Hero.vue
+++ src/components/JobSearch/Hero.vue
@@ -16,27 +16,52 @@
         <div class="col-start-12 col-span-1"></div>
       </div>
     </section>
+    <Spotlight class="spotlight">
+      <template #default="slotProps">
+        <RouterLink to="/jobs/results" class="spotlight-link">
+          <img :src="slotProps.spotlight.img" class="spotlight-image" />
+          <div class="spotlight-text-container">
+            <h3 class="spotlight-title">
+              {{ slotProps.spotlight.title }}
+            </h3>
+            <p class="spotlight-description">
+              {{ slotProps.spotlight.description }}
+            </p>
+          </div>
+          <RouterLink to="/jobs/results" class="spotlight-jobs-link"
+            >See jobs</RouterLink
+          >
+        </RouterLink>
+      </template>
+    </Spotlight>
   </main>
 </template>
 
 <script>
 import Headline from "@/components/JobSearch/Headline.vue";
 import JobSearchForm from "@/components//JobSearch/JobSearchForm.vue";
+import Spotlight from "@/components/JobSearch/Spotlight.vue";
 
 export default {
   name: "Hero",
   components: {
     Headline,
     JobSearchForm,
+    Spotlight,
   },
 };
 </script>
 
 <style lang="scss" scoped>
+main {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+
 .hero {
   display: flex;
   flex-direction: column;
-  height: 91.5vh;
   padding-top: 10px;
   padding-bottom: 80px;
 }
@@ -49,5 +74,56 @@ img {
   align-self: center;
   justify-self: center;
   margin-left: 6rem;
+}
+
+.spotlight {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  padding-bottom: 4rem;
+}
+
+.spotlight-link {
+  display: flex;
+  flex-direction: column;
+  margin-left: 1.25rem;
+  margin-right: 1.25rem;
+  border-radius: 0.5rem;
+  width: 18rem;
+  height: 24rem;
+  background-color: #f8f9fa;
+}
+
+.spotlight-text-container {
+  height: 12rem;
+  padding-left: 1.5rem;
+  padding-right: 1.5rem;
+  padding-top: 1rem;
+  padding-bottom: 1rem;
+  margin-top: 0.75rem;
+}
+
+.spotlight-title {
+  font-size: 1.125rem;
+  line-height: 1.75rem;
+  font-weight: 500;
+}
+
+.spotlight-description {
+  margin-top: 0.75rem;
+  font-size: 0.875rem;
+}
+
+.spotlight-jobs-link {
+  padding-left: 1.5rem;
+  padding-right: 1.5rem;
+  padding-bottom: 1rem;
+  font-size: 0.875rem;
+  color: #1967d2;
+}
+
+.spotlight-image {
+  margin: 0;
+  object-fit: contain;
 }
 </style>
