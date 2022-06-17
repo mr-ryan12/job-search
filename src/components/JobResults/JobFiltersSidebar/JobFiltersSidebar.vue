4083e8bbd4d5ce7af7503a498e7e3b9455a6a452
Ryan McBride <mr.ryan444@gmail.com>
2022-06-16 20:28:28 -0600

    Create component

--- src/components/JobResults/JobFiltersSidebar/JobFiltersSidebar.vue
+++ src/components/JobResults/JobFiltersSidebar/JobFiltersSidebar.vue
@@ -10,36 +10,7 @@
       <Accordion header="Things n stuff">
         <h3>Doin stuff with the things</h3>
       </Accordion>
-      <Accordion header="Organization">
-        <div class="checkbox-container">
-          <fieldset>
-            <ul class="list-container">
-              <li class="list-element">
-                <input id="VueTube" type="checkbox" />
-                <label for="VueTube" class="label">VueTube</label>
-              </li>
-              <li class="list-element">
-                <input id="Vue and Me" type="checkbox" />
-                <label for="Vue and Me" class="label">Vue and Me</label>
-              </li>
-              <li class="list-element">
-                <input id="Et Vue Brute" type="checkbox" />
-                <label for="Et Vue Brute" class="label">Et Vue Brute</label>
-              </li>
-              <li class="list-element">
-                <input id="Vue and a half men" type="checkbox" />
-                <label for="Vue and a half men" class="label"
-                  >Vue and a half men</label
-                >
-              </li>
-              <li class="list-element">
-                <input id="Vue stuff" type="checkbox" />
-                <label for="Vue stuff" class="label">Vue stuff</label>
-              </li>
-            </ul>
-          </fieldset>
-        </div>
-      </Accordion>
+      <JobFiltersSidebarOrganizations />
     </section>
   </div>
 </template>
@@ -47,11 +18,13 @@
 <script>
 import ActionButton from "@/components/Shared/ActionButton.vue";
 import Accordion from "@/components/Shared/Accordion.vue";
+import JobFiltersSidebarOrganizations from "@/components/JobResults/JobFiltersSidebar/JobFiltersSidebarOrganizations.vue";
 export default {
   name: "JobFiltersSidebar",
   components: {
     ActionButton,
     Accordion,
+    JobFiltersSidebarOrganizations,
   },
 };
 </script>
@@ -86,24 +59,5 @@ export default {
   display: flex;
   align-items: center;
   font-weight: 600;
-}
-
-.checkbox-container {
-  margin-top: 1.25rem;
-}
-
-.list-container {
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-}
-
-.list-element {
-  width: 50%;
-  height: 2rem;
-}
-
-.label {
-  margin-left: 1rem;
 }
 </style>
