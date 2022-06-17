3a07c7b131aec564449d1c7e7d5bf80e99f5f49d
Ryan McBride <mr.ryan444@gmail.com>
2022-06-16 18:36:07 -0600

    Add and style filter header

--- src/components/JobResults/JobFiltersSidebar/JobFiltersSidebar.vue
+++ src/components/JobResults/JobFiltersSidebar/JobFiltersSidebar.vue
@@ -1,10 +1,25 @@
 <template>
-  <div class="job-filter-sidebar">job filters sidebar</div>
+  <div class="job-filter-sidebar">
+    <section class="what-do-you-want-to-do-container">
+      <div class="what-do-you-want-to-do-header">
+        <h3 class="header-text">What do you want to do?</h3>
+        <ActionButton
+          text="Clear Filters"
+          type="secondary"
+          class="action-button"
+        />
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
+import ActionButton from "@/components/Shared/ActionButton.vue";
 export default {
   name: "JobFiltersSidebar",
+  components: {
+    ActionButton,
+  },
 };
 </script>
 
@@ -16,5 +31,25 @@ export default {
   background-color: #fff;
   border-right: 1px solid #dadcd0;
   width: 24rem;
+}
+
+.what-do-you-want-to-do-container {
+  padding-bottom: 1.25rem;
+}
+
+.what-do-you-want-to-do-header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+}
+
+.header-text {
+  font-weight: 900;
+}
+
+.action-button {
+  font-weight: 600;
 }
 </style>
