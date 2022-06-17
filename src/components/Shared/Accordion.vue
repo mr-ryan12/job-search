0e5622abafae6d617f11c60835658de0a0588bae
Ryan McBride <mr.ryan444@gmail.com>
2022-06-16 19:08:07 -0600

    Add arrow icons

--- src/components/Shared/Accordion.vue
+++ src/components/Shared/Accordion.vue
@@ -1,9 +1,54 @@
 <template>
-  <div>My Accordion</div>
+  <div class="accordion-container" @click="toggleContainer">
+    <div class="header-container">
+      <h3>Organizations</h3>
+      <font-awesome-icon
+        v-if="!isOpen"
+        :icon="['fas', 'angle-down']"
+        class="down-icon"
+      />
+      <font-awesome-icon v-else :icon="['fas', 'angle-up']" class="up-icon" />
+    </div>
+
+    <div v-if="isOpen" class="open-contents">Child</div>
+  </div>
 </template>
 
 <script>
 export default {
   name: "Accordion",
+  data() {
+    return {
+      isOpen: false,
+    };
+  },
+  methods: {
+    toggleContainer() {
+      this.isOpen = !this.isOpen;
+    },
+  },
 };
 </script>
+
+<style lang="scss" scoped>
+.accordion-container {
+  padding-top: 1.25rem;
+  padding-bottom: 1.25rem;
+  border-bottom: 10px solid #f8f9fa;
+  &:hover {
+    cursor: pointer;
+  }
+}
+
+.header-container {
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.open-contents {
+  width: 100%;
+  margin-top: 1.25rem;
+}
+</style>
