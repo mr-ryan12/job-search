a891276f1e0b17851b7ff873e4c07cac300bae7f
Ryan McBride <mr.ryan444@gmail.com>
2022-06-17 17:04:35 -0600

    Add getter method

--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,7 @@ import getJobs from "@/api/getJobs";
 export const LOGIN_USER = "LOGIN_USER";
 export const RECEIVE_JOBS = "RECEIVE_JOBS";
 export const FETCH_JOBS = "FETCH_JOBS";
+export const UNIQUE_ORGANIZATIONS = "UNIQUE_ORGANIZATIONS";
 
 export const state = () => {
   return {
@@ -19,6 +20,14 @@ export const mutations = {
   },
   [RECEIVE_JOBS](state, jobs) {
     state.jobs = jobs;
+  },
+};
+
+export const getters = {
+  [UNIQUE_ORGANIZATIONS](state) {
+    const uniqueOrganizations = new Set();
+    state.jobs.forEach((job) => uniqueOrganizations.add(job.organization));
+    return uniqueOrganizations;
   },
 };
 
