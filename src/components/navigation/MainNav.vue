876692ec427b5914151519207083bc5edb336678
Ryan McBride <mr.ryan444@gmail.com>
2022-06-16 20:56:11 -0600

    Add teams route

--- src/components/navigation/MainNav.vue
+++ src/components/navigation/MainNav.vue
@@ -55,7 +55,7 @@ export default {
   data() {
     return {
       menuItems: [
-        { text: "Teams", url: "/" },
+        { text: "Teams", url: "/teams" },
         { text: "Locations", url: "/" },
         { text: "Life at Ryan Corp", url: "/" },
         { text: "How we hire", url: "/" },
--- src/router/index.js
+++ src/router/index.js
@@ -5,11 +5,13 @@ const JobResultsView = () =>
   import(/* webpackChunkName: "jobs" */ "@/views/JobResultsView.vue");
 const JobView = () =>
   import(/* webpackChunkName: "jobs" */ "@/views/JobView.vue");
+const TeamsView = () => import("@/views/TeamsView.vue");
 
 const routes = [
   { path: "/", name: "Home", component: HomeView },
   { path: "/jobs/results", name: "JobResults", component: JobResultsView },
   { path: "/jobs/results/:id", name: "JobListing", component: JobView },
+  { path: "/teams", name: "TeamsView", component: TeamsView },
 ];
 
 const router = createRouter({
