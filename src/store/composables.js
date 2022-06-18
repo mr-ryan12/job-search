fb330b0881f1aa765c902f9835decdc65034f635
Ryan McBride <mr.ryan444@gmail.com>
2022-06-18 17:57:23 -0600

    Add composable

--- src/store/composables.js
+++ src/store/composables.js
@@ -1,9 +1,23 @@
 import { computed } from "vue";
 import { useStore } from "vuex";
 
-import { FILTER_JOBS } from "./constants";
+import {
+  FILTER_JOBS,
+  UNIQUE_JOB_TYPES,
+  UNIQUE_ORGANIZATIONS,
+} from "./constants";
 
 export const useFilteredJobs = () => {
   const store = useStore();
   return computed(() => store.getters[FILTER_JOBS]);
+};
+
+export const useUniqueJobTypes = () => {
+  const store = useStore();
+  return computed(() => store.getters[UNIQUE_JOB_TYPES]);
+};
+
+export const useUniqueOrganizations = () => {
+  const store = useStore();
+  return computed(() => store.getters[UNIQUE_ORGANIZATIONS]);
 };
--- tests/unit/composables/composables.test.js
+++ tests/unit/composables/composables.test.js
@@ -2,6 +2,7 @@ import { useStore } from "vuex";
 jest.mock("vuex");
 
 import { useFilteredJobs } from "@/store/composables.js";
+import { useUniqueJobTypes, useUniqueOrganizations } from "@/store/composables";
 
 describe("composables", () => {
   describe("useFilteredJobs", () => {
@@ -13,6 +14,32 @@ describe("composables", () => {
       });
       const result = useFilteredJobs();
       expect(result.value).toEqual([{ id: 1 }]);
+    });
+  });
+
+  describe("useUniqueJobtypes", () => {
+    it("Retrieves unique job types from store", () => {
+      useStore.mockReturnValue({
+        getters: {
+          UNIQUE_JOB_TYPES: new Set(["Full-time"]),
+        },
+      });
+
+      const result = useUniqueJobTypes();
+      expect(result.value).toEqual(new Set(["Full-time"]));
+    });
+  });
+
+  describe("useUniqueOrganizations", () => {
+    it("retrieves unique job organizations from store", () => {
+      useStore.mockReturnValue({
+        getters: {
+          UNIQUE_ORGANIZATIONS: new Set(["Apple"]),
+        },
+      });
+
+      const result = useUniqueOrganizations();
+      expect(result.value).toEqual(new Set(["Apple"]));
     });
   });
 });
