e6eea2793cbbadd94854aacd2a168b5d8889dc33
Ryan McBride <mr.ryan444@gmail.com>
2022-06-18 13:24:21 -0600

    Add getters tests

--- tests/unit/store/getters.test.js
+++ tests/unit/store/getters.test.js
@@ -70,4 +70,23 @@ describe("Getters", () => {
       });
     });
   });
+
+  describe("FILTER_JOBS", () => {
+    it("Filters jobs by organization and job type", () => {
+      const INCLUDE_JOB_BY_ORGANIZATION = jest.fn().mockReturnValue(true);
+      const INCLUDE_JOB_BY_JOB_TYPE = jest.fn().mockReturnValue(true);
+      const mockGetters = {
+        INCLUDE_JOB_BY_ORGANIZATION,
+        INCLUDE_JOB_BY_JOB_TYPE,
+      };
+      const job = { id: 1, title: "Best job ever" };
+      const state = {
+        jobs: [job],
+      };
+      const result = getters.FILTER_JOBS(state, mockGetters);
+      expect(result).toEqual([job]);
+      expect(INCLUDE_JOB_BY_ORGANIZATION).toHaveBeenCalledWith(job);
+      expect(INCLUDE_JOB_BY_JOB_TYPE).toHaveBeenCalledWith(job);
+    });
+  });
 });
