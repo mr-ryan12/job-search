6e826a4ec12b9a320b6adbdfa46ad35dd5261e5b
Ryan McBride <mr.ryan444@gmail.com>
2022-06-19 14:58:07 -0600

    Fetch degrees

---
+++ tests/unit/api/getDegrees.test.ts
@@ -0,0 +1,35 @@
+import axios from "axios";
+jest.mock("axios");
+
+import getDegrees from "@/api/getDegrees";
+
+const axiosGetMock = axios.get as jest.Mock;
+
+describe("getJobs", () => {
+  beforeEach(() => {
+    axiosGetMock.mockResolvedValue({
+      data: [
+        {
+          id: 1,
+          degree: "Master's",
+        },
+      ],
+    });
+  });
+
+  it("Should fetch degrees", async () => {
+    await getDegrees();
+    expect(axios.get).toHaveBeenCalledWith("http://mytotallyfakeapi.com/degrees");
+  });
+
+  it("Should extract degrees from response", async () => {
+    const response = await getDegrees();
+
+    expect(response).toEqual([
+      {
+        id: 1,
+        degree: "Master's",
+      },
+    ]);
+  });
+});
