ccdf6a6b63630d151d58f9bfee92ebc85bca92aa
Ryan McBride <mr.ryan444@gmail.com>
2022-06-19 11:09:08 -0600

    Import Commit interface

--- src/store/actions.ts
+++ src/store/actions.ts
@@ -1,8 +1,16 @@
 import { FETCH_JOBS, RECEIVE_JOBS } from "./constants";
 import getJobs from "@/api/getJobs";
 
+import { Job } from "@/api/types";
+import { Commit } from "vuex";
+
+interface Context {
+  // commit: (RECEIVE_JOBS: string, jobListings: Job[]) => void;
+  commit: Commit;
+}
+
 const actions = {
-  [FETCH_JOBS]: async (context) => {
+  [FETCH_JOBS]: async (context: Context) => {
     const jobListings = await getJobs();
     context.commit(RECEIVE_JOBS, jobListings);
   },
