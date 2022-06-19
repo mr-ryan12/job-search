befe777d0087241b32e3fe6cd812bcd065ff778d
Ryan McBride <mr.ryan444@gmail.com>
2022-06-19 12:55:44 -0600

    Fix ts errors

--- src/components/JobSearch/Spotlight.vue
+++ src/components/JobSearch/Spotlight.vue
@@ -6,19 +6,27 @@
   </ul>
 </template>
 
-<script>
+<script lang="ts">
+import { defineComponent } from "vue";
 import { ref, onMounted } from "vue";
-
 import axios from "axios";
-export default {
+
+interface Spotlight {
+  id: number;
+  img: string;
+  title: string;
+  description: string;
+}
+
+export default defineComponent({
   name: "Spotlight",
   setup() {
-    const spotlights = ref([]);
+    const spotlights = ref<Spotlight[]>([]);
 
     const getSpotlights = async () => {
       const baseUrl = process.env.VUE_APP_API_URL;
       const url = `${baseUrl}/spotlights`;
-      const response = await axios.get(url);
+      const response = await axios.get<Spotlight[]>(url);
       spotlights.value = response.data;
     };
 
@@ -26,5 +34,5 @@ export default {
 
     return { spotlights };
   },
-};
+});
 </script>
